4b1ac482b0f80574d9b74d9e6c4a2eebab20d6a6
strutly <290616329@qq.com>
2022-05-11 17:17:42 +0800

    样式修改

--- pages/article/textImage.js
+++ pages/article/textImage.js
@@ -6,6 +6,7 @@ Page({
    * 页面的初始数据
    */
   data: {
+    domain:Api.domain,
     article:{},
   },
 
@@ -17,11 +18,42 @@ Page({
     let articleRes = await Api.getArticleDetail({
       id:66
     });
-    console.log(articleRes)
+    console.log(articleRes);
+    
+    let bingfangRes = await Api.getArticle({
+      catalogId:34
+    });
+    let tuanduiRes = await Api.getArticle({
+      catalogId:35
+    })
+    let ziyuanRes = await Api.getArticleDetail({
+      id:68
+    });
+
+    that.setData({
+      article:articleRes.data,
+      ziyuan:ziyuanRes.data,
+      bingfang:bingfangRes.data.content,
+      tuandui:tuanduiRes.data.content
+    })
+
   },  
   onShow() {
     that.setData({
       systemFontSize:wx.getStorageSync('systemFontSize')||"14px"
+    })
+  },
+  onReady(){
+    that.video = that.selectComponent("#video");
+  },
+  playVideo(e){
+    console.log(e);
+    that.video.playVideo(e.currentTarget.dataset.url);
+    
+  },
+  previewImage(e){
+    wx.previewImage({
+      urls: [e.currentTarget.dataset.url],
     })
   }
 })
